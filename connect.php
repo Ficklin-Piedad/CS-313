ea9b081759356bbe77b597f1dc04c1494381c4d5
Piedad <piedad@Piedads-MacBook-Pro.local>
2016-01-26 20:59:49 -0800

    added connect.php test mode

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+
+echo 'host:$dbHost:$dbPort dbName:$dbName user:$dbUser';
+$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+$dbName = 'php';
+
+?>
